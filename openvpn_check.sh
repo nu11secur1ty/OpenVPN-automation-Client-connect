3a92513b0e1bd6a2ec1a1ae9a99eabfecf12fecf
nu11secur1ty <venvaropt@gmail.com>
2023-11-08 14:57:55 +0200

    Create openvpn_check.sh

---
+++ openvpn_check.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+# by V.Varbanovski
+PATH=/home/vpnpblx/vpn:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+squid=squid.service
+openvpn=openvpn.service
+
+# OpenVpn
+if (( $(ps -ef | grep -v grep | grep $openvpn | wc -l) > 0 ))
+then
+	echo “$openvpn is running!!!”
+else
+	systemctl start openvpn.service
+	#openvpn.service start
+	# Do not edit if you don't know WTF
+	cd /home/vpnnord0/vpnnord0/
+	#bash koi.sh
+	perl ipup.pl >> openvpn_check.log
+fi      
+	systemctl status openvpn.service
+
+# Squid
+	if (( $(ps -ef | grep -v grep | grep $squid | wc -l) > 0 ))
+then
+        echo “$squid is running!!!”
+else
+        systemctl start squid.service
+        #squid.service start
+fi
+        systemctl status squid.service
+
+exit 0;
